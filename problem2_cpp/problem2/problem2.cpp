1370c385bac0b1c2622ab5a6aa18d712734b5176
sarkar94s <soham4894@gmail.com>
2025-09-19 17:32:41 -0700

    Adding problem2_cpp

---
+++ problem2_cpp/problem2/problem2.cpp
@@ -0,0 +1,62 @@
+// problem2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int* returnProductList(int* num, int usersize) {
+    int* val = new int[usersize];
+    for (int i = 0; i < usersize; i++) {
+        int mul = 1;
+        for (int j = 0; j < usersize; j++) {
+            if (j != i) {
+                mul = mul*num[j];
+            }
+        }
+        val[i] = mul;
+    }
+    return val;
+}
+
+
+int main()
+{
+    const int SIZE = 100;
+    int usersize;
+    cout << "Enter the array size" << endl;
+    cin >> usersize;
+
+    if (usersize > SIZE) {
+        cerr << "Array Requested too Large";
+    }
+
+
+    int* num = new int[usersize];
+    cout << "Enter the numbers in your array: " << endl;
+    for (int i = 0;i < usersize;i++) {
+        cin >> num[i];
+    }
+
+    cout << "Here is the resultant array: " << endl;
+
+    int* result = returnProductList(num, usersize);
+
+    
+    for (int n = 0;n < usersize;n++) {
+        cout << result[n] << endl;
+    }
+
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
